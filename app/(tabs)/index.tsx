a1efeca65a43d3280a5cc73e9af8d7ac8af8f730
shinheeje <dbfk2637@naver.com>
2025-03-08 23:22:08 +0900

    View 테스트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,40 @@
-import { Image, StyleSheet, Platform, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function HomeScreen() {
-  return <View></View>;
+  const [text, setText] = useState("");
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>텍스트</Text>
+      </View>
+      <TextInput
+        value={text}
+        onChangeText={(value) => setText(value)}
+        style={styles.input}
+      ></TextInput>
+      <Button title="버튼이름" onPress={() => console.log("버튼이름")}></Button>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    color: "red",
+  },
+  text: {
+    fontSize: 30,
+    color: "red",
+  },
+  input: {
+    fontSize: 30,
+  },
+});
