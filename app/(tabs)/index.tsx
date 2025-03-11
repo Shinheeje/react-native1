6b7c3c7c12202429f2484949a8bd7cb3eb6a0614
shinheeje <dbfk2637@naver.com>
2025-03-11 18:56:51 +0900

    StyleSheet 테스트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,23 +12,33 @@ export default function HomeScreen() {
   const [text, setText] = useState("");
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.text}>텍스트</Text>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
       </View>
-      <TextInput
-        value={text}
-        onChangeText={(value) => setText(value)}
-        style={styles.input}
-      ></TextInput>
-      <Button title="버튼이름" onPress={() => console.log("버튼이름")}></Button>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     backgroundColor: "yellow",
     color: "red",
+  },
+  container2: {
+    backgroundColor: "blue",
+    color: "blue",
   },
   text: {
     fontSize: 30,
