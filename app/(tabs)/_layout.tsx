26cc4a1c53c327b8583f2cbe9c3d388546cabbbd
shinheeje <dbfk2637@naver.com>
2025-03-14 19:05:05 +0900

    vector-icons사용해서 TabBar Icon 변경

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { colors } from "@/constants";
 import { Tabs } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
@@ -15,18 +16,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "홈",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              size={25}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
