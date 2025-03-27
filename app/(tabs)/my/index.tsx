a3a0fc49a5048f4963ad0f6dcd8f779156520353
shinheeje <dbfk2637@naver.com>
2025-03-27 19:03:00 +0900

    로그인/회원가입 폼 핸들링

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,8 +1,14 @@
-import { router } from "expo-router";
+import { router, useFocusEffect } from "expo-router";
 import { useState } from "react";
 import { Text, SafeAreaView } from "react-native";
 
 export default function MyScreen() {
+  //useFocusEffect 화면이 포커스 됐을대 작업을 넣어 줄 수 있음
+
+  useFocusEffect(() => {
+    router.replace("/auth");
+  });
+
   return (
     <SafeAreaView>
       <Text>내정보 스크린</Text>
